8869454b4680d1e8ef34dc982f0dfc4f7b0879c6
Lee.Hyunsup <clocktree@live.co.kr>
2020-12-07 14:08:13 +0900

    20201207_2
    - 랜딩 부분 액션 보강.

--- js/randing.js
+++ js/randing.js
@@ -5,6 +5,9 @@ $(function(){
 	var fadeout_time = 1000;
 	var back_num;
 	var $randing_animate = $('.randing-animation');
+	var $catch_wrap = $('.catch-wrap');
+	var $message_background = $('.message-background');
+
 	$(document).ready(function(){
 		function phon_act1(){
 			randing_on = setTimeout(function(){
@@ -26,15 +29,24 @@ $(function(){
 				var randing_num = $randing_animate.find('.randing').length;
 
 				$randing_animate.find('.randing_1').stop().animate({'opacity':'1','z-index':'1'},'300');
+				$catch_wrap.find('.catch-1').stop().animate({'opacity':'1','z-index':'1'},'300');
+				$message_background.find('.message-1').stop().animate({'opacity':'1','z-index':'1'},'300');
+
 				randing_on = setInterval(function(){
 					$randing_animate.find('.randing_'+back_num).stop(false,false).animate({'opacity':'0','z-index':'-1'},fadeout_time);
+					$catch_wrap.find('.catch-'+back_num).stop(false,false).animate({'opacity':'0','z-index':'-1'},fadeout_time);
+					$message_background.find('.message-'+back_num).stop(false,false).animate({'opacity':'0','z-index':'-1'},fadeout_time);
 					if (randing_num>back_num){
 						back_num++;
 						$randing_animate.find('.randing_'+back_num).stop(false,false).animate({'opacity':'1','z-index':'2'},fadein_time);
+						$catch_wrap.find('.catch-'+back_num).stop(false,false).animate({'opacity':'1','z-index':'1'},fadein_time);
+						$message_background.find('.message-'+back_num).stop(false,false).animate({'opacity':'1','z-index':'1'},fadein_time);
 					}else{
 						back_num = 1;
 						// $randing_animate.stop().fadeOut('300').removeClass('back-'+back_num);
 						$randing_animate.find('.randing_'+back_num).stop(false,false).animate({'opacity':'1','z-index':'2'},fadein_time);
+						$catch_wrap.find('.catch-'+back_num).stop(false,false).animate({'opacity':'1','z-index':'1'},fadein_time+300);
+						$message_background.find('.message-'+back_num).stop(false,false).animate({'opacity':'1','z-index':'1'},fadein_time+300);
 					}
 				},randing_time1);
 			};
